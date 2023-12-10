36f891eb7c351d532bfc34f53f599ea40b5a32bc
vi1i4m <viliamboris06@gmail.com>
2023-12-10 10:43:57 +0100

    pridal som hamburger menu

--- script2.js
+++ script2.js
@@ -8,6 +8,26 @@ function myFunction() {
     }
   }
 
-  function hambur(x) {
-    x.classList.toggle("change");
+function hambur(x) {
+  x.classList.toggle("change");
+}
+
+
+function open_close(){
+  var hamb_nav = document.getElementById('hamb-nav');
+
+  if (hamb_nav.style.display === 'block'){
+    hamb_nav.style.display = 'none';
   }
+
+  else{
+    hamb_nav.style.display = 'block';
+  }
+}
+
+function zavri(){
+  var hamb_nav = document.getElementById('hamb-nav');
+  var x = document.getElementById('cont');
+  hamb_nav.style.display = 'none';
+  x.classList.remove('change')
+}
